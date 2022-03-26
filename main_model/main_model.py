66d08c0fab5378599737232bd62aed79d3646e56
tsoydeniis <94935175+tsoydeniis@users.noreply.github.com>
2022-03-26 08:52:14 +0300

    1

--- main_model/main_model.py
+++ main_model/main_model.py
@@ -11,6 +11,7 @@ distance_between_seats = 3 # расстояне между креслами
 
 
 class road():
+    '''общий объект дорога'''
     def __init__(self, number, long, out_list):
         self.number = number # индекс текущего элемента в соответствующем массиве
         self.local_list_of_agents = [-1] * long # список с агентами, находящимися на дороге
@@ -23,32 +24,48 @@ class road():
             list_of_passengers[i].move()
 
 
+    def can_i_add_one():
+        global road_width
+        for i in range(0, road_width):
+            if (self.local_list_of_agents[i] != -1):
+                return False
+        return True
+
+
 class main_road(road):
+    '''главная дорога (к другим дорогам)'''
     def __init__(self, number, long, outs):
         road.__init__(self, number, long, outs)
 
 
-    def add_new(self):
-        pass
-
-
-    def remove(self):
-        pass
+    def add_new(self, number):
+        self.local_list_of_agents[0] = number
 
 
 class long_roads(road):
+    '''дорога к креслам'''
     def __init__(self, number, long, out_list):
         road.__init__(self, number, long, out_list)
+
+
+    def add_new(self, number):
+        pass
+
+
+    def remove_one(self, position):
+        pass
 
 
 class passenger():
     def __init__(self, number):
         global ambition_list
         self.number = number
-        self.position = [0, 0, 0] # x, y, z
-        self.long = random.choice([2, 3, 4]) # не точно (хз)
+        self.position = [0, 0, 3] # x, y, z  #предпологается совпадение
+        self.long = random.choice([2, 3, 4, 5]) # не точно (хз)
         self.speed = random.choice([1, 2, 3, 4]) # не точно (хз)
         self.time_to_sit = random.choice([2, 3, 4, 5, 6, 7, 8, 9]) # не точно (хз)
+        self.time_to_bags = random.choice([4, 5, 6, 7, 8, 9, 10, 11]) # не точно (хз)
+        self.oll_time = self.time_to_sit + self.time_to_bags
         self.condition = 0  # 0 - идёт, 1 - садится, 2 - сидит
         self.ambition = ambition_list[self.number]
 
@@ -58,23 +75,52 @@ class passenger():
         #print(list_of_roads[self.position[0]][self.position[2]].out_list)
         #print(len(list_of_roads[self.position[0]][self.position[2]].local_list_of_agents))
         move_it_to = 0
-        for i in range(self.position[2], self.position[2] + self.speed + road_width):
+        if (self.condition == 0):
             if (self.position[0] == 0):
-                if
-
-
-
-
-
-
-
-
-
-
+                if (self.position[1] >= list_of_roads[0][0].out_list[self.ambition[0]]):
+                    # тут выполняем поворот (перемещение в другой массив)
+                    pass
+                elif (list_of_roads[0][0].out_list[self.ambition[0]] - list_of_roads[0][0].local_list_of_agents[self.position[1]] > self.speed):
+                    # тут шаг
+                    list_of_roads[0][0].local_list_of_agents[self.position[1]] = -1
+                    self.position[1] += self.speed
+                    #print(self.position)
+                    list_of_roads[0][0].local_list_of_agents[self.position[1]] = self.number
+                else:
+                    # тут явное перемещение
+                    self.position[1] = list_of_roads[0][0].out_list[self.ambition[0]]
+            else:
+                if (self.position[2] >= list_of_roads[1][self.ambition[0]].out_list[self.ambition[1]]):
+                    self.condition = 1
+                elif (list_of_roads[1][self.ambition[0]].out_list[self.ambition[1]] - self.position[1] > self.speed):
+                    # тут шаг
+                    list_of_roads[1][self.position[1]].local_list_of_agents[self.position[1]] = -1
+                    self.position[1] += self.speed
+                    #print(self.position)
+                    list_of_roads[0][0].local_list_of_agents[self.position[1]] = self.number
+                    pass
+                else:
+                    # тут явное перемещение
+                    self.position[2] = list_of_roads[1][self.ambition[0]].out_list[self.ambition[1]]
+        elif (self.condition == 1):
+            self.oll_time -= 1
+            if (self.oll_time <= 0):
+                self.condition = 2
 
 
     def get_last_point(self):
         return (self.position[1] - self.long)
+
+
+def let_one_in():
+    global on_board_now
+    global ambition_list
+    global list_of_roads
+    global list_of_passengers
+    if (on_board_now < len(ambition_list) and list_of_roads[0][0].can_i_add_one()):
+        list_of_passengers.append(passenger(on_board_now))
+        list_of_roads[0][0].add_new(on_board_now)
+        on_board_now += 1
 
 
 def make_outs_list_1():
