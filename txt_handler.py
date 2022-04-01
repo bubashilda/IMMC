07e643be6e614ffe347444c0f08c4b38448b4c98
bubashilda <72609321+bubashilda@users.noreply.github.com>
2022-04-01 22:33:51 +0300

    delete some files

--- txt_handler.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('/Users/glebspivakovskij/Documents/GitHub/IMMC/models/res.csv', sep=';')
-df.to_excel('/Users/glebspivakovskij/Documents/GitHub/IMMC/models/res.csv', index=False)
